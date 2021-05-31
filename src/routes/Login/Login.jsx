178f71eac8f817fbc8dba54d57ed046dbbfed8ff
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-05-31 12:05:52 +0530

    feat: Initial changes for Login Page

---
+++ src/routes/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./login.css";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="login__left">dadw</div>
+      <div className="login__right">awdawd</div>
+    </div>
+  );
+};
