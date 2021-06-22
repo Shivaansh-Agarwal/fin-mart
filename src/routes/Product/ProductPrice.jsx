13ebe5243b3b57f3961151f0d9ada19c02424281
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-06-22 22:34:49 +0530

    feat: Change Add To Cart button text and functionality to Open Cart if item already in cart, feat: Commented out Buy Now button since functionality not present

--- src/routes/Product/ProductPrice.jsx
+++ src/routes/Product/ProductPrice.jsx
@@ -4,8 +4,10 @@ import styles1 from "./styles/Product.module.css";
 import styles2 from "./styles/ProductPrice.module.css";
 import { MdShoppingCart } from "react-icons/md";
 import { useProductsContext } from "../../contexts/products.context.js";
+import { useNavigate } from "react-router";
 
 export const ProductPrice = ({ id, price, inStock }) => {
+  const navigate = useNavigate();
   const { productsState, productsDispatch } = useProductsContext();
 
   const { discount, discountPercentage, discountedPrice, originalPrice } =
@@ -16,20 +18,30 @@ export const ProductPrice = ({ id, price, inStock }) => {
   const inStockString = inStock ? "In Stock" : "Out of Stock";
   const inStockClassName = inStock ? styles2.instock : styles2.outofstock;
 
-  function addToCartHandler() {
-    productsDispatch({
-      type: "ADD_TO_CART",
-      payload: id,
-    });
-    toast.info("Book Added to Cart 😃", {
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+  const { cartList } = productsState;
+  const isPresentInCart = cartList.find((item) => item.id === id)
+    ? true
+    : false;
+  const btnCartText = isPresentInCart ? " Open Cart" : " Add To Cart";
+
+  function cartHandler() {
+    if (isPresentInCart) {
+      navigate("/cart");
+    } else {
+      productsDispatch({
+        type: "ADD_TO_CART",
+        payload: id,
+      });
+      toast.info("Book Added to Cart 😃", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   }
 
   return (
@@ -51,14 +63,14 @@ export const ProductPrice = ({ id, price, inStock }) => {
           <button
             className={styles2.btnaddtocart}
             disabled={!inStock}
-            onClick={addToCartHandler}
+            onClick={cartHandler}
           >
             <MdShoppingCart />
-            {` Add To Cart`}
+            {btnCartText}
           </button>
-          <button className={styles2.btnbuynow} disabled={!inStock}>
+          {/* <button className={styles2.btnbuynow} disabled={!inStock}>
             Buy Now
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
