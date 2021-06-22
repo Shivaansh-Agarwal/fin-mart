e7d3e6785fa2401d51c7133702df2a2e8e1f4134
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-06-22 22:46:47 +0530

    feat: Navigate to Product Page onClick of Book Cover Image or Book Name in Cart

--- src/routes/Cart/CartItemCard.jsx
+++ src/routes/Cart/CartItemCard.jsx
@@ -3,6 +3,7 @@ import styles from "./styles/CartItem.module.css";
 import { MdDelete } from "react-icons/md";
 import { IoHeart } from "react-icons/io5";
 import { ProductCartQuantity } from "../../components";
+import { useNavigate } from "react-router";
 
 export const CartItemCard = ({
   id,
@@ -15,15 +16,21 @@ export const CartItemCard = ({
   inStock,
   productsDispatch,
 }) => {
+  const navigate = useNavigate();
+  const navigateToProductPage = () => {
+    navigate(`/products/${id}`);
+  };
   const { discountPercentage, discountedPrice, originalPrice } = price;
   return (
     <div className={styles.cartItem}>
       <div className={styles.cartItemDetails}>
-        <div className={styles.cartImage}>
+        <div className={styles.cartImage} onClick={navigateToProductPage}>
           <img src={image} alt="Product in Cart" />
         </div>
         <div className={styles.cartRight}>
-          <div className={styles.bookname}>{name}</div>
+          <div className={styles.bookname} onClick={navigateToProductPage}>
+            {name}
+          </div>
           <div className={styles.bookauthor}>{author}</div>
           <div className={styles.bookbinding}>{description}</div>
           <div className={styles.priceWrapper}>
